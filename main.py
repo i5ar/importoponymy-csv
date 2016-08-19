d5071ca32bde3dc9bdc4e78e22ea9f751682a54a
iSar <i5ar@live.it>
2016-08-19 02:13:39 +0200

    Added columns count Possible to have more columns in the sic.csv file

--- main.py
+++ main.py
@@ -1,59 +1,70 @@
 # -*- coding: utf-8 -*-
-import csv
+from __future__ import print_function
 from builtins import input
 from six import iteritems
+import csv
 
-# Source
-# path_gis = 'in-gis.csv'
-# path_sic = 'in-sic.csv'
-path_gis = input("Inserisci il nome del file CSV esportato dal GIS (completo di percorso ed estensione): ")
-path_sic = input("Inserisci il nome del file CSV esportato dal SIC (completo di percorso ed estensione): ")
-# Destination
+print("Quando ti verra' chiesto di inserire i nomi dei files \
+\nricorda di inserire anche l'estensione oltre che il percorso.", end="\n")
+
+# path_gis = 'gis.csv'
+# path_sic = 'sic.csv'
 # path_out = 'out.csv'
-path_out = input("Inserisci il nome del file CSV di output (completo di percorso ed estensione): ")
+# path_dup = 'dup.csv'
+
+path_gis = input("Inserisci il nome del file CSV esportato dal GIS [data/gis.csv]: ") or "data\\gis.csv"
+path_sic = input("Inserisci il nome del file CSV esportato dal SIC [data/sic.csv]: ") or "data\\sic.csv"
+path_out = input("Inserisci il nome del file CSV di output [out.csv]: ") or "out.csv"
+path_dup = input("Inserisci il nome del file CSV dei toponomi duplicati (facoltativo): ") or None
+
+def list_address(sic_len, field, froad):
+    '''Define the address list based on the columns number'''
+    address = []
+    for i in range(sic_len):
+        if i == 0:
+            address.append(froad)
+            address.append(field[i])
+        else:
+            address.append(field[i])
+    return address
 
 with open(path_gis, 'r') as r:
     spreadsheet = csv.reader(r, delimiter=',')
-    dic = {} # key: value
+    gis = {}
     for row in spreadsheet:
         # Append "key: value" to dictionary
-        dic[row[1] +' '+ row[2]] = row[0]
+        gis[row[1] +' '+ row[2]] = row[0]
 r.closed
 
 with open(path_sic, 'r') as h:
     spreadsheet = csv.reader(h, delimiter=',')
-    lst = []
-    for row in spreadsheet:
-        # Append tuple ("area di circolazione", "numero", "esponente") to list
-        lst.append((row[0] +' '+ row[1], row[2], row[3], row[4]))
+    sic = tuple(spreadsheet)
+    sic_len = len(sic[0])
 h.closed
 
-# Comparison "SIC: GIS" names
-# names = {
-#     'contrada santa maria': 'contrada s.maria',
-# }
-path_names = input("Inserisci il nome del file CSV delle associazioni dei toponimi (completo di percorso ed estensione): ")
-with open(path_names, 'r') as n:
-    spreadsheet = csv.reader(n, delimiter=',')
-    names = {} # key: value
-    for row in spreadsheet:
-        # Append "key: value" to dictionary
-        names[row[0]] = row[1]
-n.closed
+# Names comparison
+# nam = {'contrada santa maria': 'contrada s.maria'}
+if path_dup:
+    with open(path_dup, 'r') as n:
+        spreadsheet = csv.reader(n, delimiter=',')
+        nam = {}
+        for row in spreadsheet:
+            nam[row[0]] = row[1]
+    n.closed
 
 with open(path_out, 'w') as o:
     writer = csv.writer(o, delimiter=',', lineterminator='\n')
-    for field in lst:
-        if field[0] in dic.keys():
+    for field in sic:
+        if field[0]+' '+field[1] in gis.keys():
             # Get dictionary value from "area di circolazione" key
-            area_id = dic[field[0]]
+            froad = gis[field[0]+' '+field[1]]
             # Write "area id", "area di circolazione", "numero", "esponente"
-            writer.writerow([area_id, field[0], field[1], field[2], field[3]])
-        # Handle comparison
+            writer.writerow(list_address(sic_len, field, froad))
+        # Handle comparison with names dictionary
         else:
-            for (key, value) in iteritems(names):
-                if (field[0] == value): # 'contrada s.maria'
-                    area_id = dic[key] # dic['contrada santa maria']
-                    writer.writerow([area_id, field[0], field[1], field[2], field[3]])
-
+            if path_dup:
+                for (key, value) in iteritems(nam):
+                    if (field[0]+' '+field[1] == value): # 'contrada s.maria'
+                        froad = gis[key] # gis['contrada santa maria']
+                        writer.writerow(list_address(sic_len, field, froad))
 o.closed
